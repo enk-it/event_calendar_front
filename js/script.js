02684b222bd1b6b0de38cba7424fbc9b5313fc12
enk-it <gladysh.danil@gmail.com>
2024-08-08 11:35:25 +0700

    feat: Add styles for secondary days

--- js/script.js
+++ js/script.js
@@ -1,24 +1,47 @@
 import {set_text_today, mark_event, unmark_event, mark_main, mark_secondary, set_text, clear_days} from "./elements.js";
 import {get_current_month_year, get_next_month_year, get_previous_month_year, get_month_days, get_calendar_ready} from "./date_time.js";
 
-function main() {
-    const days = document.getElementsByClassName(
-        "cell"
-    )
 
+const days = document.getElementsByClassName(
+    "cell"
+)
+
+const month_select = document.getElementById("month")
+const year_select = document.getElementById("year")
+const reset_span = document.getElementById("reset")
+
+reset_span.addEventListener('click', () => {reset()})
+month_select.addEventListener('change', () => {
+    update_calendar(
+        {
+            month: parseInt(month_select.value),
+            year: parseInt(year_select.value)
+        }
+    )
+})
+year_select.addEventListener('change', () => {
+    update_calendar(
+        {
+            month: parseInt(month_select.value),
+            year: parseInt(year_select.value)
+        }
+    )
+})
+
+function set_options ({month, year}) {
+    month_select.value = month.toString()
+    year_select.value = year.toString()
+}
+
+
+function update_calendar(month_year) {
     clear_days(days)
 
-
-    const _current_my = get_current_month_year()
-    const current_my = get_next_month_year(_current_my)
-
-
-
-    const next_my = get_next_month_year(current_my)
-    const previous_my = get_previous_month_year(current_my)
+    const next_my = get_next_month_year(month_year)
+    const previous_my = get_previous_month_year(month_year)
 
     const previous_month_days = get_month_days(previous_my)
-    const current_month_days = get_month_days(current_my)
+    const current_month_days = get_month_days(month_year)
     const next_month_days = get_month_days(next_my)
 
     const rendered_calendar = get_calendar_ready(
@@ -38,19 +61,27 @@ function main() {
         if (current_day.main === true) {
             mark_main(current_day_element)
         }
+        else {
+            mark_secondary(current_day_element)
+        }
         if (current_day.today === true) {
             set_text_today(current_day_element, "Сегодня")
+        }
+        else {
+            set_text_today(current_day_element, "")
         }
         // activate(current_day_element)
         set_text(current_day_element, current_day.date)
     }
 }
 
-/*
-* написать метод который по паре месяц-год возвращал бы все дни месяца
-* написать метод который бы возвращал по месяцу-году (два числа) готовый календарь)
-*/
 
-console.log("asdas")
+function reset() {
+    const current_my = get_current_month_year()
+    set_options(current_my)
+    update_calendar(current_my)
+}
 
-main()
+
+
+reset()
